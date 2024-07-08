2ce6a075069ea808d16a69b049c0cfa49afa7c42
Toghrul89 <116531144+Toghrul89@users.noreply.github.com>
2024-07-08 15:24:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,103 +1,35 @@
-import express from 'express';
-import { Book } from "./models/book.js";
+import http from 'http';
+import fs from 'fs';
 
-const app = express();
-
-app.set("port", process.env.PORT || 3000);
-app.use(express.static('./public')); // allows direct navigation to static files
-app.use(express.urlencoded()); //Parse URL-encoded bodies
-app.use(express.json()); //Used to parse JSON bodies
-
-import cors from 'cors';
-app.use('/api', cors());
-
-// set template engine
-app.set("view engine", "ejs");
-
-app.get('/', (req,res, next) => {
-    res.render('home');
-});
-
-app.get('/detail', (req,res,next) => {
-    Book.findOne({ title:req.query.title }).lean()
-        .then((book) => {
-            res.render('details', {result: book} );
-        })
-        .catch(err => next(err));
-});
-
-app.get('/about', (req,res) => {
-    res.type('text/html');
-    res.render('about');
-});
-
-// api's
-app.get('/api/v1/book/:title', (req, res, next) => {
-    let title = req.params.title;
-    Book.findOne({title: title}, (err, result) => {
-        if (err || !result) {
-            res.status(404).json({"message":"not found"});
-        } else {
-            res.json( result );
-         }
-    });
-});
-
-app.get('/api/v1/book/:id', (req, res, next) => {
-    Book.findOne({_id: req.params.id}, (err, result) => {
-        if (err || !result) {
-            res.status(404).json({"message":"not found"});
-        } else {
-            res.json( result );
-         }
-    });
-});
-
-app.get('/api/v1/books', (req,res, next) => {
-    Book.find({}).lean()
-    .then((books) => {
-        res.json(books);
-    })
-    .catch(err => res.json({"error": err}));
-});
-
-app.get('/api/v1/delete/:id', (req,res, next) => {
-    Book.deleteOne({"_id":req.params.id })
-    .then(result => {
-        res.json({"deleted": result});
-    })
-    .catch(err => res.json({"error": err}));
-});
-
-app.post('/api/v1/add/', (req,res, next) => {
-    // find & update existing item, or add new
-    if (!req.body._id) { // insert new document
-        Book.create(req.body).then(result => res.json(result))
-        .catch(err => res.json({"error": err}));
-    } else { // update existing document
-        Book.updateOne({ _id: req.body._id}, req.body, {upsert:true})
-        .then(result => res.json(result))
-        .catch(err => res.json({"error": err}));
-    }
-});
-
-// less reliable method
-app.get('/api/v1/add/:title/:author/:pubdate', (req,res, next) => {
-    // find & update existing item, or add new 
-    let title = req.params.title;
-    Book.update({ title: title}, {title:title, author: req.params.author, pubdate: req.params.pubdate }, {upsert: true }, (err, result) => {
-        if (err) return next(err);
-        // nModified = 0 for new item, = 1+ for updated item 
-        res.json({updated: result.nModified});
-    });
-});
-
-app.use((req,res) => {
-    res.type('text/plain'); 
-    res.status(404);
-    res.send('404 - Not found');
-});
-
-app.listen(app.get('port'), () => {
-    console.log('Express started');    
-});
+function serveStatic(res, path, contentType, responseCode){
+  if(!responseCode) responseCode = 200;
+  console.log(__dirname + path)
+  fs.readFile(__dirname + path, function(err, data){
+      if(err){
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Internal Server Error');
+      }
+      else{
+        res.writeHead(responseCode, {'Content-Type': contentType});
+        res.end(data);
+      }
+  });
+}
+http.createServer(function(req,res){
+  console.log('createServer got request')
+  var path = req.url.toLowerCase();
+  switch(path) {
+    case '/': 
+      serveStatic(res, '/../public/home.html', 'text/html');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About Toghrul's Project');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('404:Page not found.');
+  }
+  
+}).listen(process.env.PORT || 3000);
+console.log('after createServer')
