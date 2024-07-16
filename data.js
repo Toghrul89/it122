e0b4dd86bb9b9a60ebdc4026330d9ac1d4075462
Toghrul89 <116531144+Toghrul89@users.noreply.github.com>
2024-07-15 18:56:46 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 const items = [
-  { id: 1, booktitle: 'Slowness', author: 'Milan Kundera', publicationhdate: '1996' },
-  { id: 1, booktitle: 'Death with Interruptions', author: 'Jose Saramago', publicationhdate: '2005' },
-  { id: 1, booktitle: 'Slowness', author: 'Milan Kundera', publicationhdate: '1996' },
-  { id: 1, booktitle: 'Slowness', author: 'Milan Kundera', publicationhdate: '1996' },
-  { id: 1, booktitle: 'Slowness', author: 'Milan Kundera', publicationhdate: '1996' },
+  { id: 1, title: 'Slowness', author: 'Milan Kundera', publicationhdate: '1996' },
+  { id: 1, title: 'Death with Interruptions', author: 'Jose Saramago', publicationhdate: '2005' },
+  { id: 1, title: 'Emma', author: 'Jane Austen', publicationhdate: '1815' },
+  { id: 1, title: 'Animal Farm', author: 'George Orwell', publicationhdate: '1945' },
+  { id: 1, title: 'Letter to My Daughter', author: 'Maya Angelou', publicationhdate: '2008' },
 ];
 
 export function getAll() {
