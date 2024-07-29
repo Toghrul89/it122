cc0b3a95849da5b6b592d1a551b77f5762de6ff0
Toghrul89 <116531144+Toghrul89@users.noreply.github.com>
2024-07-28 22:28:17 -0700

    Update book.js

--- models/book.js
+++ models/book.js
@@ -1,23 +1,11 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-import { connectionString } from './config/db.js';
-
-mongoose.connect(connectionString, {
-    dbName: 'tjaffarov',
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-mongoose.connection.on('open', () => {
-  console.log('Mongoose connected.');
-});
-
 const bookSchema = new Schema({
- title: { type: String, required: true },
- author: String,
- publicationdate: Number,
- genre: String
+  title: { type: String, required: true },
+  author: String,
+  publicationdate: Number,
+  genre: String
 });
 
 export const Book = mongoose.model('Book', bookSchema);
