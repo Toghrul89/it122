b2ba6e1b7cd2a24b5678d5b843069566ab824a94
Toghrul89 <116531144+Toghrul89@users.noreply.github.com>
2024-07-28 21:50:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 import express from 'express';
-import { getAll, getItem } from './data.js';
+import { Book } from './models/book.js';
+import connectDB from './config/db.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+connectDB();
+
 app.set('view engine', 'ejs');
 app.use(express.static('public')); 
 
-app.get('/', (req, res) => {
-  const books = getAll();
-  res.render('home', { books });
+app.get('/', (req, res, next) => {
+  Book.find({}).lean()
+    .then((books) => {
+      res.render('home', { books });
+    })
+    .catch(err => next(err));
 });
 
-app.get('/detail', (req, res) => {
-  const book = getItem(req.query.id);
-  if (book) {
-    res.render('detail', { book });
-  } else {
-    res.status(404).send('Book not found');
-  }
+app.get('/detail', (req, res, next) => {
+  Book.findOne({ _id: req.query.id }).lean()
+    .then((book) => {
+      res.render('detail', { book });
+    })
+    .catch(err => next(err));
+});
+
+app.get('/delete', (req, res, next) => {
+  Book.deleteOne({ _id: req.query.id })
+    .then((result) => {
+      if (result.deletedCount > 0) {
+        res.send('Delete succeeded');
+      } else {
+        res.send('Delete failed: Book not found');
+      }
+    })
+    .catch(err => next(err));
 });
 
 app.listen(port, () => {
