6f66190745cf49b886108c573a9200d1d9903d8f
Toghrul89 <116531144+Toghrul89@users.noreply.github.com>
2024-07-28 22:59:45 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectionString = "mongodb+srv://tjaffarov:sUkPG5IkmAvmV35m@cluster0.drly
 export const connectDB = async () => {
   try {
     await mongoose.connect(connectionString, {
-      dbName: 'SCCPROJECT',
+      dbName: 'books',
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
