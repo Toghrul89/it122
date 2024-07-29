da20b46eb7f4c36b1101fc432d6ad8b0ae07d915
Toghrul89 <116531144+Toghrul89@users.noreply.github.com>
2024-07-28 21:59:56 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,18 @@
-export const connectionString = "mongodb+srv://<dbuser>:<dbpassword>@<cluster>.mongodb.net/test?retryWrites=true";
+import mongoose from 'mongoose';
+
+const connectionString = "mongodb+srv://tjaffarov:sUkPG5IkmAvmV35m@cluster0.drlyzrq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(connectionString, {
+      dbName: 'tjaffarov',
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
