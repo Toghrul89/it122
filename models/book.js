725f083e0e12d12c65729e3edd877c849064e36c
Toghrul89 <116531144+Toghrul89@users.noreply.github.com>
2024-07-28 21:04:53 -0700

    Update book.js

--- models/book.js
+++ models/book.js
@@ -1,1 +1,12 @@
+import mongoose from 'mongoose';
 
+const bookSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  publicationdate: Number,
+  genre: String,
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+export default Book;
